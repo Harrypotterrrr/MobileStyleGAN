e1a36e70a5109be82d81c92c532a9323ab4b3cc2
Harrypotterrrr <jiahaolin19971119@126.com>
2022-04-21 15:49:39 -0500

    add linearTran2d for discriminator

--- models/modules/utils.py
+++ models/modules/utils.py
@@ -2,8 +2,9 @@ import math
 import tensorflow as tf
 import tensorflow.keras as keras
 
-from models.modules import *
+from tensorflow import nn
 
+from models.modules.upfirdn import upfirdn2d
 
 def make_kernel(k):
     k = tf.constant(k, dtype=tf.float32)
@@ -26,7 +27,7 @@ class PixelNorm(keras.Model):
         )
 
 
-class NonlinearTransform(keras.Model):
+class LinearTransform(keras.Model):
 
     def __init__(
             self,
@@ -37,7 +38,7 @@ class NonlinearTransform(keras.Model):
             lr_mul = 1,
             activation = None,
     ):
-        super(NonlinearTransform, self).__init__()
+        super(LinearTransform, self).__init__()
 
         # TODO: lr_mul, math.sqrt(in_dim) meaning
         self.scale = (1 / math.sqrt(in_ch)) * lr_mul
@@ -45,7 +46,7 @@ class NonlinearTransform(keras.Model):
         self.weight = self.weight * self.scale
 
         if bias:
-            self.bias = tf.Variable(tf.fill(in_ch, bias_init), dtype=tf.float32)
+            self.bias = tf.Variable(tf.fill(out_ch, bias_init), dtype=tf.float32)
             self.bias = tf.reshape(self.bias, [1, out_ch])
             self.bias = self.bias * lr_mul
         else:
@@ -60,6 +61,36 @@ class NonlinearTransform(keras.Model):
         #  out = fused_leaky_relu(out, self.bias * self.lr_mul, onnx_trace=self.onnx_trace)
         if self.activation:
             out = tf.nn.leaky_relu(out, alpha=0.2)
+        return out
+
+
+class LinearTransform2d(keras.Model):
+
+    def __init__(
+            self,
+            in_ch,
+            out_ch,
+            kernel_size,
+            stride = 1,
+            padding =0,
+            bias = True,
+    ):
+        super(LinearTransform2d, self).__init__()
+
+        self.stride = stride
+        self.padding = padding
+
+        self.scale = (1 / math.sqrt(in_ch * kernel_size ** 2))
+        self.weight = tf.Variable(tf.random.normal([in_ch, out_ch]))
+        self.weight = self.weight * self.scale
+
+        if bias:
+            self.bias = tf.Variable(tf.zeros([out_ch], dtype=tf.float32))
+        else:
+            self.bias = 0.0
+
+    def call(self, style):
+        out = nn.conv2d(style, self.weight, bias=self.bias, strides=self.stride, padding=self.padding)
         return out
 
 
@@ -84,7 +115,7 @@ class ConstantInput(keras.Model):
     def __init__(self, channel, size=4):
         super(ConstantInput, self).__init__()
         # TODO, change tf.Variable to tf.constant
-        self.const_input = tf.constant(tf.random.normal([1, channel, size, size]))
+        self.const_input = tf.constant(tf.random.normal([1, size, size, channel]))
 
     def call(self, x):
         out = tf.tile(self.const_input, [x.shape[0], 1, 1, 1])
@@ -99,37 +130,12 @@ class Upsample(keras.Model):
         self.factor = factor
         self.kernel = make_kernel(kernel) * (factor ** 2)
 
-        pad = kernel.shape[0] - factor
+        pad = self.kernel.shape[0] - factor
         self.pad = ((pad + 1) // 2 + factor - 1, pad // 2)
 
     def call(self, x):
         return upfirdn2d(x, self.kernel, up=self.factor, down=1, pad=self.pad)
 
-
-class ToRGB(keras.Model):
-
-    def __init__(
-            self,
-            in_channel,
-            style_dim,
-            upsample=True,
-            blur_kernel=[1, 3, 3, 1]
-    ):
-        super(ToRGB, self).__init__()
-
-        if upsample:
-            self.upsample = Upsample(blur_kernel)
-
-        self.conv = ModulatedConv(in_channel, 3, 1, style_dim, demodulate=False)
-        self.bias = tf.Variable(tf.zeros([1, 3, 1, 1]))
-
-    def call(self, x, style, skip=None):
-        out = self.conv(x, style)
-        out = out + self.bias
-        if skip is not None:
-            skip = self.upsample(skip)
-            out = out + skip
-        return out
 
 class NoiseInjection(keras.Model):
 
@@ -140,6 +146,7 @@ class NoiseInjection(keras.Model):
 
     def call(self, x, noise=None):
         if noise is None:
-            noise = tf.random.normal(x.shape)
+            B, _, H, W = x.shape
+            noise = tf.random.normal([B, 1, H, W])
         out = x + self.weight * noise
         return out
